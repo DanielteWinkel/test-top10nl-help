b865976a80af9975099057d34855c35ebc387e2c
Daniël te Winkel <daniel.tewinkel@kadaster.nl>
2023-09-26 15:08:25 +0200

    Update analyse.py

--- todo_daniel/convert/analyse.py
+++ todo_daniel/convert/analyse.py
@@ -8,14 +8,18 @@ starttijd = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 # De volgorde van deze stappen is belangrijk, voer ze van boven naar beneden uit
 
 # invoermap opgeven
-dir_in = r'E:\Daniel\GITea-checkout\dpigi-TOP10NL-help\Top10NL Help Verkenningsvoorschriften\Content'
+# dir_in = r'E:\Daniel\GITea-checkout\dpigi-TOP10NL-help\Top10NL Help Verkenningsvoorschriften\Content'
+dir_in = r'E:\Daniel\GITea-checkout\test-top10nl-help\verkenningsvoorschriften'
 
 #uitvoer bestanden opgeven
 dir_out = r'E:\Daniel\GITea-checkout\test-top10nl-help\todo_daniel\convert' #deze repository
-filenaam_snippet_out = "Snippet-analyse_" + (starttijd.replace(" ", "_")).replace(":",".") + ".txt"
-filenaam_keyword_out = "Keyword-analyse_" + (starttijd.replace(" ", "_")).replace(":",".") + ".txt"
+filenaam_snippet_out = (starttijd.replace(" ", "_")).replace(":",".") + "_Snippet-analyse" + ".txt"
+filenaam_keyword_out = (starttijd.replace(" ", "_")).replace(":",".") + "_Keyword-analyse" + ".txt"
 file_snippet_out = os.path.join(dir_out, filenaam_snippet_out)
 file_keyword_out = os.path.join(dir_out, filenaam_keyword_out)
+
+# encoding opgeven
+enc = 'utf-8'
 
 # voor een map (in dit geval alleen hoofdmap A)
 # for files in glob.glob("**/A.htm", recursive=True):
@@ -35,20 +39,20 @@ f_out = open(file_snippet_out, 'w')
 f_out.write ("--------------------------------------------------------------------------------------------------------------------------------\n\n")
 f_out.write ("Analyse uitgevoerd op: %s\n\n" % (starttijd))
 f_out.write ("--------------------------------------------------------------------------------------------------------------------------------\n\n")
-f_out.write ("Path;Bestandsnaam;Snippet\n")
+f_out.write ("Path en Bestandsnaam;Snippet\n")
 
 os.chdir(dir_in)
 # voor de hele alfabetmap:
 for files in glob.glob("**/*.htm", recursive=True):
     file = os.path.join(dir_in, files)
-    print(file, end=" ")
+    #print(file, end=" ")
     # invoer bestanden openen
-    for regel in fileinput.input([files]):
-        element = regel.split()
-        snippet = 'test'
+    for regel in fileinput.input(files=[files],openhook=fileinput.hook_encoded("utf-8", "surrogateescape")):
+        if "flsnp" in regel:
+            snippet = regel
+            f_out.write ("%s;%s" % (file, snippet))
     fileinput.close()
-    print('  klaar')
-    f_out.write ("%s;%s;%s\n" % (dir_in, bestandsnaam, snippet))
+    #print('  klaar')
        
 #sluiten bestanden
 f_out.close()
@@ -70,14 +74,14 @@ os.chdir(dir_in)
 # voor de hele alfabetmap:
 for files in glob.glob("**/*.htm", recursive=True):
     file = os.path.join(dir_in, files)
-    print(file, end=" ")
+    #print(file, end=" ")
     # invoer bestanden openen
-    for regel in fileinput.input([files]):
-        element = regel.split()
-        keyword = 'test'
+    for regel in fileinput.input(files=[files],openhook=fileinput.hook_encoded("utf-8", "surrogateescape")):
+        if "MadCap:keyword" in regel:
+            keyword = regel
+            f_out.write ("%s;%s" % (file, keyword))
     fileinput.close()
-    print('  klaar')
-    f_out.write ("%s;%s;%s\n" % (dir_in, bestandsnaam, keyword))
+    #print('  klaar')
        
 #sluiten bestanden
 f_out.close()
