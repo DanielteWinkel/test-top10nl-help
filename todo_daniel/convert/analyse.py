346d034eaa9b822b4e64d2d77641a0b4b4c6a401
Daniël te Winkel <daniel.tewinkel@kadaster.nl>
2023-09-26 14:11:23 +0200

    Update analyse.py

--- todo_daniel/convert/analyse.py
+++ todo_daniel/convert/analyse.py
@@ -12,8 +12,10 @@ dir_in = r'E:\Daniel\GITea-checkout\dpigi-TOP10NL-help\Top10NL Help Verkenningsv
 
 #uitvoer bestanden opgeven
 dir_out = r'E:\Daniel\GITea-checkout\test-top10nl-help\todo_daniel\convert' #deze repository
-filenaam_out = "Snippet-analyse_" + (starttijd.replace(" ", "_")).replace(":",".") + ".txt"
-file_out = os.path.join(dir_out, filenaam_out)
+filenaam_snippet_out = "Snippet-analyse_" + (starttijd.replace(" ", "_")).replace(":",".") + ".txt"
+filenaam_keyword_out = "Keyword-analyse_" + (starttijd.replace(" ", "_")).replace(":",".") + ".txt"
+file_snippet_out = os.path.join(dir_out, filenaam_snippet_out)
+file_keyword_out = os.path.join(dir_out, filenaam_keyword_out)
 
 # voor een map (in dit geval alleen hoofdmap A)
 # for files in glob.glob("**/A.htm", recursive=True):
@@ -24,16 +26,17 @@ file_out = os.path.join(dir_out, filenaam_out)
 # voor de hele alfabetmap:
 # for files in glob.glob("**/*.htm", recursive=True):
 
-#openen uitvoer bestand
-f_out = open(file_out, 'w')
+# --------------------------------------------------------------------------------------------------------------------------------------------------
+# Analyseer HTM bestanden op het gebuik van Snippets
+
+# openen uitvoer bestand
+f_out = open(file_snippet_out, 'w')
 
 f_out.write ("--------------------------------------------------------------------------------------------------------------------------------\n\n")
 f_out.write ("Analyse uitgevoerd op: %s\n\n" % (starttijd))
 f_out.write ("--------------------------------------------------------------------------------------------------------------------------------\n\n")
 f_out.write ("Path;Bestandsnaam;Snippet\n")
 
-# --------------------------------------------------------------------------------------------------------------------------------------------------
-# Analyseer HTM bestanden op het gebuik van Snippets
 os.chdir(dir_in)
 # voor de hele alfabetmap:
 for files in glob.glob("**/*.htm", recursive=True):
@@ -47,11 +50,39 @@ for files in glob.glob("**/*.htm", recursive=True):
     print('  klaar')
     f_out.write ("%s;%s;%s\n" % (dir_in, bestandsnaam, snippet))
        
-    
+#sluiten bestanden
+f_out.close()
     
 # --------------------------------------------------------------------------------------------------------------------------------------------------
 
+# --------------------------------------------------------------------------------------------------------------------------------------------------
+# Analyseer HTM bestanden op het gebuik van Keywords
+
+# openen uitvoer bestand
+f_out = open(file_keyword_out, 'w')
+
+f_out.write ("--------------------------------------------------------------------------------------------------------------------------------\n\n")
+f_out.write ("Analyse uitgevoerd op: %s\n\n" % (starttijd))
+f_out.write ("--------------------------------------------------------------------------------------------------------------------------------\n\n")
+f_out.write ("Path;Bestandsnaam;Keyword\n")
+
+os.chdir(dir_in)
+# voor de hele alfabetmap:
+for files in glob.glob("**/*.htm", recursive=True):
+    file = os.path.join(dir_in, files)
+    print(file, end=" ")
+    # invoer bestanden openen
+    for regel in fileinput.input([files]):
+        element = regel.split()
+        keyword = 'test'
+    fileinput.close()
+    print('  klaar')
+    f_out.write ("%s;%s;%s\n" % (dir_in, bestandsnaam, keyword))
+       
 #sluiten bestanden
 f_out.close()
+    
+# --------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 # dir_in = r'E:\Daniel\GITea-checkout\dpigi-TOP10NL-help\Top10NL-Help\Content'
